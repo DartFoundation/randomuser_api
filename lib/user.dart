9b35d3662417f010722216cc07320216c1fe967d
ZARIFMAC <uzbzarif@gmail.com>
2023-08-04 15:42:29 +0500

    random User

--- lib/user.dart
+++ lib/user.dart
@@ -4,12 +4,21 @@ class RandomUser {
   final String email;
   final String phone;
 
-
+// Constructor
   RandomUser({
     required this.firstName,
     required this.lastName,
     required this.email,
     required this.phone,
   });
+// Convert JSON to RandomUser object
+  factory RandomUser.fromJson(Map<String, dynamic> json) {
+    return RandomUser(
+      firstName: json['results'][0]['name']['first'],
+      lastName: json['results'][0]['name']['last'],
+      email: json['results'][0]['email'],
+      phone: json['results'][0]['phone'],
+    );
+  }
 
 }
