b83eaff15a109d29c5c66dade6a03f8b44a8ec4c
ZARIFMAC <uzbzarif@gmail.com>
2023-08-04 15:22:09 +0500

    get first and last name

---
+++ lib/user.dart
@@ -0,0 +1,15 @@
+class RandomUser {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String phone;
+
+
+  RandomUser({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.phone,
+  });
+
+}
