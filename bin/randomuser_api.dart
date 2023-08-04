8d3758a13ea6914f147bc7f41890f2a04c690951
ZARIFMAC <uzbzarif@gmail.com>
2023-08-04 15:02:14 +0500

    async request

--- bin/randomuser_api.dart
+++ bin/randomuser_api.dart
@@ -6,6 +6,15 @@ Uri url = Uri(
     path: '/api',
 
 );
-// Print url
-print(url);
+// Print the info about starting the request.
+print('Starting request to ${url.toString()}');
+// Make the asyncronous HTTP request:
+http.get(url).then((response) {
+  //  Print the status code of the response.
+  print(response.statusCode);
+  // Print the info about the finished request.
+  print('Request complete.');
+});
+// Print the info that the request is sent.
+print('Request sent!');
 }
