c354d6e2356f547beaf39d17db6f90f8ff9ffbc0
ZARIFMAC <uzbzarif@gmail.com>
2023-08-04 15:25:52 +0500

    get request future

--- bin/randomuser_api.dart
+++ bin/randomuser_api.dart
@@ -1,6 +1,7 @@
 import 'package:http/http.dart' as http;
-
-void makeRequest() async {
+import 'package:randomuser_api/user.dart';
+import 'dart:convert';
+Future<RandomUser> makeRequest() async {
   Uri url = Uri(
     scheme: 'https',
     host: 'randomuser.me',
@@ -11,10 +12,28 @@ http.Response response = await http.get(url);
 // Print status code
 print(response.statusCode);
 // Print response body
-print(response.body);
+Map<String, dynamic> jsonResponse = jsonDecode(response.body);
 
+String firstName = jsonResponse['results'][0]['name']['first'];
+String lastName = jsonResponse['results'][0]['name']['last'];
+
+// Create RandomUser object
+RandomUser user = RandomUser(
+  firstName: firstName,
+  lastName: lastName,
+  email: jsonResponse['results'][0]['email'],
+  phone: jsonResponse['results'][0]['phone'],
+);
+return user;
 }
 
+
+
 void main() {
-  makeRequest();
+  makeRequest().then((RandomUser user) {
+    print(user.firstName);
+    print(user.lastName);
+    print(user.email);
+    print(user.phone);
+  });
 }
